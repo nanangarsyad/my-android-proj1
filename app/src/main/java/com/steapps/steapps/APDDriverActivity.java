edfbdc0fe988c4f85606bfca3e114790ab03d566
nanangarsyad <nanangarsyad@gmail.com>
2017-06-29 08:46:03 +0700

    first commit

---
+++ app/src/main/java/com/steapps/steapps/APDDriverActivity.java
@@ -0,0 +1,13 @@
+package com.steapps.steapps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class APDDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apddriver);
+    }
+}
